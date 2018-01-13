cf519d9bb018ab9f07f759c6b42f7d888df200e4
YassineGhoumari <32045731+YassineGhoumari@users.noreply.github.com>
2018-01-13 17:15:10 -0500

    Create Animal.java

---
+++ JavaFun/Animal.java
@@ -0,0 +1,37 @@
+public class Animal{
+
+
+    private int limbs;
+    private int legs;
+    private String name;
+    private String species;
+    private boolean gender;
+
+
+
+    public Animal(int limbs,int b,String c,String d,boolean e){
+
+        limbs = a;
+        legs = b;
+        name = c;
+        species = d;
+        gender = e;
+
+            
+    }
+
+
+    public void setlimbs(int limbs){
+        this.limbs = limbs;
+    }
+
+
+    
+
+    public int getlimbs(){
+        return limbs;
+    }
+    
+
+    
+}
