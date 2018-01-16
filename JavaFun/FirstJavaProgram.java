c8532974fd26269f66a13190931799277db3e7fe
YassineGhoumari <32045731+YassineGhoumari@users.noreply.github.com>
2018-01-15 21:46:13 -0500

    Create FirstJavaProgram.java

---
+++ JavaFun/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Yassine");
+        System.out.println("I'am 200 years old");
+        System.out.println("My hometown is Falls Chruch, VA");
+    }
+}
