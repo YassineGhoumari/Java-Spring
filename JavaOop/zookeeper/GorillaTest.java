5a6bce79eb9b97857263f7bca4165d9a591b30c0
YassineGhoumari <32045731+YassineGhoumari@users.noreply.github.com>
2018-01-26 11:43:31 -0500

    Create GorillaTest.java

---
+++ JavaOop/zookeeper/GorillaTest.java
@@ -0,0 +1,17 @@
+package com.codingdojo.zookeeper;
+
+class GorillaTest {
+
+	public static void main(String[] args) {
+	
+	Gorilla gorilla = new Gorilla();
+	gorilla.throwSomething();
+	gorilla.throwSomething();
+	gorilla.throwSomething();
+	gorilla.eatBananas();
+	gorilla.eatBananas();
+	gorilla.climb();
+	
+	}
+	
+}
